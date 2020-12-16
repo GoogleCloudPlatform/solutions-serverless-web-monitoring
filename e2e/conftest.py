c9a7461e91a0b1745b5c7025aa356e87f97fe5d8
Jacob Ferriero <jferriero@google.com>
2020-12-16 20:44:59 +0000

    chore: terraform as pytest fixture to clean up after failed e2e test

--- e2e/conftest.py
+++ e2e/conftest.py
@@ -11,6 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+import shlex
+import subprocess
+import re
+
+E2E_TEST_DIR = os.path.realpath(os.path.dirname(__file__))
+
+ANSI_ESCAPE_PATTERN = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
 
 def pytest_addoption(parser):
@@ -23,4 +31,33 @@ def pytest_addoption(parser):
   parser.addoption("--bucket_metrics", action="store", default=None)
   parser.addoption("--function_tracer", action="store", default=None)
   parser.addoption("--pubsub_subscription_alerts", action="store", default=None)
+
+@pytest.fixture(scope='module')
+def terraform_infra(request):
+
+    def _run(cmd):
+        print(
+            ANSI_ESCAPE_PATTERN.sub(
+                '',
+                subprocess.check_output(
+                    cmd,
+                    stderr=subprocess.STDOUT,
+                    cwd=E2E_TEST_DIR
+                ).decode('UTF-8')
+            )
+        )
+
+    init = shlex.split("terraform init")
+    apply = shlex.split("terraform apply -auto-approve")
+    destroy = shlex.split("terraform destroy -auto-approve")
+
+    _run(init)
+    _run(apply)
+
+    def teardown():
+        _run(destroy)
+
+    request.addfinalizer(teardown)
+    with open(os.path.join(E2E_TEST_DIR, "terraform.tfstate")) as tf_state_file:
+        return json.load(tf_state_file)
 
